17fa7da48807124dfc90d8b30a1b39ce6f73a56e
kriptonian <shymal.sawan@gmail.com>
2022-09-30 00:43:42 +0530

    feat: Setup github pages

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* change the basename in production */}
-    <BrowserRouter basename={process.env.PUBLIC_URL}> 
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
