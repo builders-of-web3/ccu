4978cf4c710b5028b173fd59da9f4b257dbb7135
raj <agentR47@gmail.com>
2022-10-08 12:17:04 +0530

    fix: Update footer image alignment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <div className={'w-screen py-16 bg-white text-black'}>
             <div className={'w-[80vw] mx-auto grid md:grid-cols-5 gap-10 md:gap-20'}>
 
-                <div className={'flex md:flex-col col-span-3 md:col-span-1 justify-between items-center'}>
+                <div className={'flex flex-col col-span-3 md:col-span-1 md:justify-between items-start md:items-center'}>
                     <img src={bow3FullLogoBlack} alt={''} className={'w-[120px] md:w-[200px]'} />
                     <a href="https://twitter.com/intent/tweet?hashtags=buidlersofweb3%2C&original_referer=https%3A%2F%2Fpublish.twitter.com%2F&ref_src=twsrc%5Etfw%7Ctwcamp%5Ebuttonembed%7Ctwterm%5Ehashtag%7Ctwgr%5Ebuidlersofweb3&text=Hey%20Guys%20%F0%9F%91%8B%2C%20I%20am%20gonna%20join%20%40buidlersofweb3%20on%20October%2029-30%20at%20Tal%20Kutir%2C%20Eco%20Space%2C%20Kolkata.%20Join%20me%20in%20%23BUIDLbyBow3">
                         <div className={'flex gap-3 items-center'}>
